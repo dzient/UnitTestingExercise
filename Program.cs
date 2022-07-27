99e6fa2498f54e5c7ddf38547593ee79d3fb961c
dzient <34776686+dzient@users.noreply.github.com>
2022-07-27 01:33:48 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//-----------------------------------------------------------------
+// ExceptionHandlingExercise
+//
+// Name: David Zientara
+// Date: 7-25-2022
+// Comments: An exercise in using exception handling
+// Made changes per the instructions
+//--------------------------------------------------------------
+
+namespace UnitTestingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
